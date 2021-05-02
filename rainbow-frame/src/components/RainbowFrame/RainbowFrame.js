dd30ba1a470fac57c41922dc4d7923727d34cfec
Vitaliy Klubkou <vitaliy.klubkou@gmail.com>
2021-05-02 17:51:45 +0300

    Function Rainbow Frame

--- rainbow-frame/src/components/RainbowFrame/RainbowFrame.js
+++ rainbow-frame/src/components/RainbowFrame/RainbowFrame.js
@@ -1,34 +1,11 @@
 import React from 'react';
-import PropTypes from 'prop-types';
 
-class RainbowFrame extends React.Component {
+export default (props) => {
+    const divs = props.colors.reduce((acc, item) => (
+        <div style={{border: `8px solid ${item}`, padding: '10px'}}>
+            {acc}
+        </div>
+    ), props.children);
 
-    static propTypes = {
-        colors: PropTypes.array.isRequired,
-    };
-
-    getDiv(index) {
-        return (
-            <div
-                key={index}
-                style={{
-                    border: `solid 4px ${this.props.colors[index]}`,
-                    padding: '10px',
-                    width: `${400 - index * 30}px`
-                }}
-            >
-                {this.props.colors.length - 1 > index ? this.getDiv(index + 1) : this.props.children}
-            </div>
-        );
-    }
-
-    render() {
-        return (
-            <div>
-                {this.getDiv(0)}
-            </div>
-        )
-    }
+    return <div>{divs}</div>;
 }
-
-export default RainbowFrame;
